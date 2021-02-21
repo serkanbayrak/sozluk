b89f0a742e4dceeb70bd8403df1bdd44ba802dbf
serkan bayrak <74434684+serkanbayrak@users.noreply.github.com>
2021-02-21 22:13:03 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+     class MyDictionary<Tip1,Tip2>
+    {
+        Tip1[] AnahtarKelime;
+        Tip2[] Ozellik;
+
+        public MyDictionary()
+        {
+            AnahtarKelime = new Tip1[0];
+            Ozellik = new Tip2[0];
+        }
+
+        public void Add(Tip1 anahtar,Tip2 ozellik)
+        {
+            Tip1[] tempTip1 = AnahtarKelime;
+            Tip2[] tempTip2 = Ozellik;
+
+            AnahtarKelime = new Tip1[tempTip1.Length + 1];
+            for (int i = 0; i < AnahtarKelime.Length; i++)
+            {
+                AnahtarKelime[i] = tempTip1[i];
+            }
+            AnahtarKelime[AnahtarKelime.Length - 1] = anahtar;
+
+            Ozellik = new Tip2[tempTip2.Length + 1];
+            for (int i = 0; i < Ozellik.Length; i++)
+            {
+                Ozellik[i] = tempTip2[i];
+            }
+            Ozellik[Ozellik.Length - 1] = ozellik;
+
+            Console.WriteLine(anahtar+"     "+ozellik);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> MyDictionary = new MyDictionary<string, string>();
+
+            MyDictionary.Add("key", "anahtar");
+            MyDictionary.Add("run","kosmak, çalıştırmak");
+            MyDictionary.Add("process", "işlem");
+
+        }
+    }
+}
